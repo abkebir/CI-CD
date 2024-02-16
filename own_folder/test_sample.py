a6c0385949af3e675b4f94ef0b5c075ef939ce93
Kébir ALLADE <alladeabdou01@gmail.com>
2024-02-16 10:29:46 +0100

    feat(test): add one more test

--- own_folder/test_sample.py
+++ own_folder/test_sample.py
@@ -1,7 +1,16 @@
-from sample import add_word,size
+from sample import add_word,size,check
 
 # Test add_word() function with an existing word
 def test_add_existing_word():
     add_word('apple')
     assert add_word('apple') == False
     assert size() == 1
+
+
+
+
+def test_check():
+    global words
+    words.update(['cat', 'dog'])
+    result = check()
+    assert result == ['cat','dog']
