eb900ee8f25439de45ade80c47a53728e1617e47
Gata <gata@Etx.lan>
2025-06-16 14:45:23 +0200

    add pre-commit fix

--- own_folder/test_sample.py
+++ own_folder/test_sample.py
@@ -1,16 +1,16 @@
-from sample import add_word,size,check
+from sample import add_word, size, check
+
 
 # Test add_word() function with an existing word
 def test_add_existing_word():
-    add_word('apple')
-    assert add_word('apple') == False
+    add_word("apple")
+    assert not add_word("apple")  # Should return False for existing word
+
     assert size() == 1
-
-
 
 
 def test_check():
     global words
-    words.update(['cat', 'dog'])
+    words.update(["cat", "dog"])
     result = check()
-    assert result == ['cat','dog']
+    assert result == ["cat", "dog"]
