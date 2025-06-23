2c72d9aa1de59cec45cb94ea4eaf91d7a410490c
kebir allade <alladeabdou01@gmail.com>
2025-06-23 18:56:36 +0200

    updating file level

---
+++ src/environment/configwriter.py
@@ -0,0 +1,23 @@
+from configparser import ConfigParser
+
+config = ConfigParser()
+
+config["dev"] = {
+    "host": "dev.example.com",
+    "port": "8080",
+    "username": "dev_user",
+    "password": "dev_password",
+    "database": "dev_db",
+}
+
+config["prod"] = {
+    "host": "prod.example.com",
+    "port": "8080",
+    "username": "prod_user",
+    "password": "prod_password",
+    "database": "prod_db",
+}
+
+with open("./environment/config.ini", "w")\
+     as configfile:
+    config.write(configfile)
---
+++ src/own_folder/sample.py
@@ -0,0 +1,26 @@
+from src.environment.configread import get_env_config
+
+
+words = set()
+
+
+def check():
+    return [word for word in words if len(word) > 2]
+
+
+def size():
+    return len(words)
+
+
+def add_word(word):
+    # Checks to see if the word is already in our dictionary. If not add it
+    if word not in words:
+        words.add(word)
+        return True
+    return False
+
+
+config = get_env_config("dev")
+print(config['port'])
+add_word("apple")
+add_word("mango")
